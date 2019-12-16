1779a6986f706f347408795fb05c26f3026689f9
itzli2000 <itzli2000@msn.com>
2019-12-15 19:21:09 -0600

    Styles Completed styles for responsive desing

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -13,4 +13,24 @@ $(document).ready(function () {
         autoplay: true,
         autoplaySpeed: 3000,
     });
+    var isMessageSent = setInterval(() => {
+        var urlActual = new URL(document.location);
+        var isSent = urlActual.searchParams.get("send");
+        if (isSent) {
+            console.log(isSent);
+            // window.history.replaceState({}, document.title, "/");
+            clearInterval(isMessageSent);
+            Swal.fire({
+                icon: 'success',
+                title: 'Contacto',
+                html: 'Su mensaje ha sido enviado, <br>Nos pondremos en contacto a la brevedad.',
+                confirmButtonColor: '#227a21'
+            }).then(response => {
+                document.location.href="/";
+            });
+        }
+        // else {
+        //     console.log('not sent');
+        // }
+    }, 500);
 });
--- contact.php
+++ contact.php
@@ -1,51 +1,50 @@
-<?php 
-if(isset($_POST['submit'])){
+<?php
+if (isset($_POST['submit'])) {
     $encoding = "utf-8";
-// Preferences for Subject field
+    // Preferences for Subject field
     $subject_preferences = array(
         "input-charset" => $encoding,
         "output-charset" => $encoding,
         "line-length" => 76,
         "line-break-chars" => "\r\n"
-        );
+    );
 
-// Multiple recipients
+    // Multiple recipients
     $to = 'itzli2000@msn.com';
     $from_name = $_POST['Name'];
     $from_phone = $_POST['Phone'];
     $from_mail = $_POST['Email'];
     $from_message = $_POST['Message'];
 
-// Subject
+    // Subject
     $subject = 'Contacto desde Erikvon';
 
-// Message
+    // Message
     $message = '
     <html>
-    <head>
-      <title>Mensaje.</title>
-  </head>
-  <body>
-      <h2>Mensaje enviado desde página personal.</h2>
-      <h4>'.$from_name.' <br>Correo: '.$from_mail.' <br>Teléfono: '. $from_phone .' <br>Escribió lo siguiente:</h4>
-      <p> Mensaje: '. $from_message .'</p>
-  </body>
-  </html>
-  ';
+        <head>
+            <title>Mensaje.</title>
+        </head>
+        <body>
+            <h2>Mensaje enviado desde página personal.</h2>
+            <h4>' . $from_name . ' <br>Correo: ' . $from_mail . ' <br>Teléfono: ' . $from_phone . ' <br>Escribió lo siguiente:</h4>
+            <p> Mensaje: ' . $from_message . '</p>
+        </body>
+    </html>
+    ';
 
-// To send HTML mail, the Content-type header must be set
-// Mail header
-  $header = "Content-type: text/html; charset=".$encoding." \r\n";
-  $header .= "De: ".$from_name.$from_lastname." <".$from_mail."> \r\n";
-  $header .= "MIME-Version: 1.0 \r\n";
-  $header .= "Content-Transfer-Encoding: 8bit \r\n";
-  $header .= "Fecha: ".date("r (T)")." \r\n";
-  $header .= iconv_mime_encode("Asunto", $mail_subject, $subject_preferences);
+    // To send HTML mail, the Content-type header must be set
+    // Mail header
+    $header = "Content-type: text/html; charset=" . $encoding . " \r\n";
+    $header .= "De: " . $from_name . $from_lastname . " <" . $from_mail . "> \r\n";
+    $header .= "MIME-Version: 1.0 \r\n";
+    $header .= "Content-Transfer-Encoding: 8bit \r\n";
+    $header .= "Fecha: " . date("r (T)") . " \r\n";
+    $header .= iconv_mime_encode("Asunto", $mail_subject, $subject_preferences);
 
-// Mail it
-  mail($to, $subject, $message, $header);
+    // Mail it
+    mail($to, $subject, $message, $header);
 
-// Redirect
-  header('Location: ready.html');
+    // Redirect
+    header('Location: index.html?send=true');
 }
-?>
